afdfb9fc746ab1521d654504f6800c7e500cc383
DonaldiDemiraj <donaldidemiraj@gmail.com>
2022-09-23 17:09:55 +0200

    contact form add

--- src/Twill/Capsules/Leads/routes/web.php
+++ src/Twill/Capsules/Leads/routes/web.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register/leads', [Leads\Twill\Capsules\Leads\Http\Controllers\Web\LeadController::class, 'store'])->name('store');
 Route::post('updated/leads/{id}', [Leads\Twill\Capsules\Leads\Http\Controllers\Web\LeadController::class, 'updated'])->name('updated');
+Route::post('delete/leads/{id}', [Leads\Twill\Capsules\Leads\Http\Controllers\Web\LeadController::class, 'delete'])->name('delete');
 
 
-Route::get('contact-us', [ContactController::class, 'index']);
-Route::post('contact-us', [ContactController::class, 'store'])->name('contact.us.store');
+Route::get('contact-us', [Leads\Twill\Capsules\Leads\Http\Controllers\Web\ContactController::class, 'index']);
+Route::post('contact-us', [Leads\Twill\Capsules\Leads\Http\Controllers\Web\ContactController::class, 'store'])->name('contact.us.store');
